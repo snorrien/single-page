6ec58aa99c24de58632b48b8c1dd9bc007638365
Alena Shapovalova <avshapovalova31@gmail.com>
2023-04-21 14:41:30 +0200

    first page

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,86 @@
+import { NavLink } from "react-router-dom";
+import CustomLink from "./CustomLink";
+import styled from 'styled-components';
+
+const MenuBlock = styled.div`
+  justify-content: space-between;
+  display: flex;
+  text-align: center;
+    vertical-align: middle;
+    align-items: center;
+`;
+
+const MenuContainer = styled.div`
+  justify-content: space-between;
+  display: flex;
+  text-align: center;
+
+`;
+
+const Menu = styled.ul`
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding-left: 130px;
+`;
+
+const MenuItem = styled.li`
+  margin: 0 30px;
+`;
+
+const MenuLink = styled.a`
+  text-decoration: none;
+  color: rgba(0, 0, 0, 1);
+  font-weight: 400;
+  background-color: transparent; 
+  caret-color: transparent; 
+  cursor: pointer; 
+  white-space: nowrap;
+
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: rgba(79, 70, 229, 1);
+    text-decoration-skip-ink: none;
+    text-underline-offset: 5px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #fbfbfb;
+  color:#4F46E5;
+  border: 1px solid #4F46E5;
+  padding: 12px 24px;
+  font-weight: 500;
+  font-size: 16px;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: #0062cc;
+    color: #fff;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+const Navbar = () => {
+  return (
+
+    <MenuBlock>
+      <MenuContainer>
+        <img src="/imgs/smartDialogs.png" alt="логотип" />
+        <Menu>
+          <MenuItem><NavLink to="/aboutUs">O нас</NavLink></MenuItem>
+          <MenuItem><MenuLink href="/platform">Платформа</MenuLink></MenuItem>
+          <MenuItem><MenuLink href="#">Портфолио</MenuLink></MenuItem>
+          <MenuItem><MenuLink href="#">Контакты</MenuLink></MenuItem>
+        </Menu>
+      </MenuContainer>
+      <Button>Вход</Button>
+    </MenuBlock>
+
+  )
+}
+
+export default Navbar;
